50b77da4a109ac44a39170dc3d99a65088ec308e
ssmith-wombatweb <ssmith@wombatweb.us>
2018-08-19 13:01:39 -0700

    Moved inline styling for the layout into styled-jsx and added a base font.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -16,15 +16,20 @@ const Layout = ({ children, data, location }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
+    <style jsx>
+      {`
+      font-family: 'Open Sans', Arial, sans-serif;
+
+      .content {
+        margin: 0 auto;
+        max-width: 960;
+        padding: 0px 1.0875rem 1.45rem;
+        paddingTop: 0;
+      }
+      `}
+    </style>
     <Header data={data} siteTitle={data.site.siteMetadata.title} location={location} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className="content">
       {children()}
     </div>
   </div>
