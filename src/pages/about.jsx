7a666d63c98e2e2ad03cae9c4abdcccfcb6ec396
ssmith-wombatweb <ssmith@wombatweb.us>
2018-08-19 07:31:58 -0700

    Added about page.

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>
+      {'Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.'}
+
+      {'Squashy armchairs dirt on your nose brass scales crush the Sopophorous bean with flat side of silver dagger, releases juice better than cutting. Full moon Whomping Willow three turns should do it lemon drops. Locomotor trunks owl treats that will be 50 points, Mr. Potter. Witch Weekly, he will rise again and he will come for us, headmaster Erumpent horn. Fenrir Grayback horseless carriages ‘zis is a chance many would die for!'}
+    </p>
+  </div>
+);
+
+export default About;
